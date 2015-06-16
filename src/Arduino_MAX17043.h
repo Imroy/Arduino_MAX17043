1e8132ffc2a381379b6794b13730ea8a237ecf2d
Ian Tester <imroykun@gmail.com>
2015-06-17 03:28:25 +1000

    Fix register addresses in enum
    These are all 16-bit registers, so their addresses are always even. Maybe an enum wasn't the best way to define them?
    This is why SoC() was returning funny values :P

--- src/Arduino_MAX17043.h
+++ src/Arduino_MAX17043.h
@@ -21,12 +21,12 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #include <stdint.h>
 
 enum MAX17043_regs {
-  VCELL = 0x02,		// R/O
-  SOC,			// R/O
-  MODE,			// W/O
-  VERSION,		// R/O
-  CONFIG = 0x0c,	// R/W
-  COMMAND = 0xfe,	// W/O
+  VCELL		= 0x02,	// R/O
+  SOC		= 0x04,	// R/O
+  MODE		= 0x06,	// W/O
+  VERSION	= 0x08,	// R/O
+  CONFIG	= 0x0c,	// R/W
+  COMMAND	= 0xfe,	// W/O
 };
 
 class Arduino_MAX17043 {
